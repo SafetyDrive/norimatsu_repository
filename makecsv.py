b6bd9fc279e4e026b9cddd0fe2d9de8760c97985
Norimatsu Takato <166518862+norimatsuhonmono@users.noreply.github.com>
2024-09-10 21:18:50 +0900

    Update makecsv.py
    加速度，座標x,y,z，をcsvファイルとしてまとめる．
    5行目の内容を各々で変更すればいけるはず

--- makecsv.py
+++ makecsv.py
@@ -7,7 +7,7 @@ com = serial.Serial("COM7", 9600)
 
 with open("test.csv", "a", newline='') as f:
     writer = csv.writer(f)
-    writer.writerow(["Timestamp", "Data1", "Data2"])
+    writer.writerow(["Timestamp", "acceleration", "co_x","co_y","co_z"])
 
 while True:
     ## from Arduino date
@@ -15,9 +15,9 @@ while True:
 
     data = val.split(",")
 
-    if len(data) == 2:
+    if len(data) == 4:
         date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         
         with open("test.csv", "a", newline='') as f:
             writer = csv.writer(f)
-            writer.writerow([date, data[0], data[1]])
+            writer.writerow([date, data[0], data[1], data[2], data[3]])
